7d3aac609437715864bfb3565d14b63ac13e3bf3
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-08-30 19:02:16 +0530

    Initial Commit

---
+++ Shinchan Catch Me If You Can/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shinchan Catch Me If You Can
+//
+//  Created by Parikshit Gothwal on 30/08/19.
+//  Copyright © 2019 Urantia Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
