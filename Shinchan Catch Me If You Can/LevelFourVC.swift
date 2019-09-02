8c34882dcc55778d79fb131b1c9b18aec893a87d
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-09-02 13:31:54 +0530

    Level Four VC Created

---
+++ Shinchan Catch Me If You Can/LevelFourVC.swift
@@ -0,0 +1,30 @@
+//
+//  LevelFourVC.swift
+//  Shinchan Catch Me If You Can
+//
+//  Created by Parikshit Gothwal on 02/09/19.
+//  Copyright © 2019 Urantia Tech. All rights reserved.
+//
+
+import UIKit
+
+class LevelFourVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
