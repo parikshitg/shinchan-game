3695a57f6b2a211d7b38d303f879918dbed16713
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-09-02 14:20:22 +0530

    level four objects designed

--- Shinchan Catch Me If You Can/LevelFourVC.swift
+++ Shinchan Catch Me If You Can/LevelFourVC.swift
@@ -10,6 +10,49 @@ import UIKit
 
 class LevelFourVC: UIViewController {
 
+    @IBOutlet weak var shinchan1: UIImageView!
+    @IBOutlet weak var shinchan2: UIImageView!
+    @IBOutlet weak var shinchan3: UIImageView!
+    @IBOutlet weak var shinchan4: UIImageView!
+    @IBOutlet weak var shinchan5: UIImageView!
+    @IBOutlet weak var shinchan6: UIImageView!
+    @IBOutlet weak var shinchan7: UIImageView!
+    @IBOutlet weak var shinchan8: UIImageView!
+    @IBOutlet weak var shinchan9: UIImageView!
+    @IBOutlet weak var shinchan10: UIImageView!
+    @IBOutlet weak var shinchan11: UIImageView!
+    @IBOutlet weak var shinchan12: UIImageView!
+    
+    @IBOutlet weak var shinchanMid: UIImageView!
+    
+    @IBOutlet weak var shiro1: UIImageView!
+    @IBOutlet weak var shiro2: UIImageView!
+    @IBOutlet weak var shiro3: UIImageView!
+    @IBOutlet weak var shiro4: UIImageView!
+    @IBOutlet weak var shiro5: UIImageView!
+    @IBOutlet weak var shiro6: UIImageView!
+    
+    @IBOutlet weak var himawari1: UIImageView!
+    @IBOutlet weak var himawari2: UIImageView!
+    @IBOutlet weak var himawari3: UIImageView!
+    @IBOutlet weak var himawari4: UIImageView!
+    @IBOutlet weak var himawari5: UIImageView!
+    @IBOutlet weak var himawari6: UIImageView!
+    
+    @IBOutlet weak var harry1: UIImageView!
+    @IBOutlet weak var harry2: UIImageView!
+    
+    @IBOutlet weak var mitsi1: UIImageView!
+    @IBOutlet weak var mitsi2: UIImageView!
+    
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +60,7 @@ class LevelFourVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func levelFourStartButton(_ sender: Any) {
     }
-    */
-
 }
